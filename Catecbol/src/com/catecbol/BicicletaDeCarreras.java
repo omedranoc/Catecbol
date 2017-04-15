c5fad326bade65c3b95f4daa96d18af54d1f3b38
Saul Medrano <saulmedrano@carfax.com>
2017-04-15 10:46:05 -0500

    polimorfismo ejemplo

--- Catecbol/src/com/catecbol/BicicletaDeCarreras.java
+++ Catecbol/src/com/catecbol/BicicletaDeCarreras.java
@@ -2,7 +2,27 @@ package com.catecbol;
 
 public class BicicletaDeCarreras extends Bicicleta  {
     String nombre;
+    int velocidad;
 
+    @Override
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public  void imprimirColor( String color){
+
+        System.out.println("El color de la bicicleta es: "+ color);
+    }
+    public  void imprimirColor( String colorMarco, String tenedor){
+
+        System.out.println("El color de la bicicleta es: "+ colorMarco +
+        " y "+ tenedor);
+    }
+
+    @Override
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad*4;
+    }
 
     public String getNombre() {
         return nombre;
--- Catecbol/src/com/catecbol/ClasePrincipal.java
+++ Catecbol/src/com/catecbol/ClasePrincipal.java
@@ -6,23 +6,14 @@ public class ClasePrincipal {
 
     public static void main(String[] args) {
 
-        Bicicleta bicicletaDeRonny =  new Bicicleta();
-        bicicletaDeRonny.setVelocidad(30);
-        bicicletaDeRonny.setCadencia(8);
-        bicicletaDeRonny.setCambios(3);
 
-        Bicicleta bicicletaDeSaul = new Bicicleta();
-        bicicletaDeSaul.setCadencia(12);
-        bicicletaDeSaul.setCambios(1);
-        bicicletaDeSaul.setVelocidad(25);
 
         BicicletaDeCarreras bicicletaDeCarreras = new BicicletaDeCarreras();
-        bicicletaDeCarreras.setNombre("Negrita");
+        //bicicletaDeCarreras.setNombre("Negrita");
         bicicletaDeCarreras.setVelocidad(40);
+        bicicletaDeCarreras.imprimirColor("blanco", "rojo");
 
-        System.out.println("velocidad: "+ bicicletaDeRonny.getVelocidad() );
-        System.out.println("velocidad: "+ bicicletaDeSaul.getVelocidad() );
-        System.out.println("la velocidad de "+bicicletaDeCarreras.getNombre()
-                + " es: "+ bicicletaDeCarreras.getVelocidad() );
+
+        System.out.println("la velocidad de es: "+ bicicletaDeCarreras.getVelocidad() );
     }
 }
